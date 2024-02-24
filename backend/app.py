715bf9cb492950f8798da873c2a720d1a4a0964f
Shimanto Bhowmik <bmikshimanto@gmail.com>
2024-02-24 14:31:08 -0500

    Timing middlware and http integration

--- backend/app.py
+++ backend/app.py
@@ -14,3 +14,24 @@ v2t = VideoToText()
 i2t = ImageToText()
 t2s = TextToSpeech()
 s2t = ScenesToText()
+
+
+# disable cors
+@app.middleware("http")
+async def add_cors_header(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+
+# timing middleware
+@app.middleware("http")
+async def add_process_time_header(request, call_next):
+    import time
+
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    print(f"Processing time: {process_time}")
+    return response
